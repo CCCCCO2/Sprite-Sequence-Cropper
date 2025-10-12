15ad21fa3d6de0b3a0be6cf40b6225beb778f754
CCCCCO2 <mingmingzi_X@163.com>
2025-10-12 19:52:45 +0800

    ✨ feat: 新增角色朝向判断
    ✨ feat: 新增角色朝向判断
    
    - 新增角色朝向判断。UI 面板中新增一个开关用于设置角色的左右朝向，该值影响最终写入 .csv 文件中的偏移值的正负。如果角色朝向为左侧，则正的偏移值代表向左偏移，负的偏移值代表向右偏移。如果角色朝向为右侧，则正的偏移值代表向右偏移，负的偏移值代表向左偏移。总之无论如何，该值的目的都是为了保证偏移值的正始终代表着角色的“正面”前进方向，负代表角色的“背后”后退方向
    - 自动裁剪(直立类生物)面板中新增 `导出所有文档锚点到CSV` 功能。将打开的所有文档的中心锚点坐标导出到 .csv 文件。主要用于对已裁剪好的图片，批量记录他们的中心坐标时使用

--- GameSeqFrameCropper.jsx
+++ GameSeqFrameCropper.jsx
@@ -8,7 +8,8 @@ var bipedAutoCropSettings=
     needCropBottom : true,
     needDrawGuideLine : true,
     toleranceOfBottom : 5,
-    isBottomCenter : true
+    isBottomCenter : true,
+    faceLeft : true
 };
 
 var manualCropSettings=
@@ -16,7 +17,8 @@ var manualCropSettings=
     needDrawGuideLine : true,
     needExportCSVFile : true,
     needHorizontalScaling: true,
-    needVerticalScaling: false
+    needVerticalScaling: false,
+    faceLeft : true
 };
 
 // ===================== UI 界面 =====================
@@ -51,14 +53,16 @@ whichInfo.selection = whichInfo.items[0];
 // ===================== 下拉菜单：手动选区裁剪 =====================
 
 // 其它 Toggle
-var needDrawGuideLineCheckbox = manualCropSettingsGroup.add("checkbox", undefined, "绘制参考线");
-var needExportCSVFileCheckbox = manualCropSettingsGroup.add("checkbox", undefined, "同时导出CSV");
-var needHorizontalScalingCheckbox = manualCropSettingsGroup.add("checkbox", undefined, "水平方向扩展");
-var needVerticalScalingCheckbox = manualCropSettingsGroup.add("checkbox", undefined, "垂直方向扩展");
-needDrawGuideLineCheckbox.value = true;
-needExportCSVFileCheckbox.value = true;
-needHorizontalScalingCheckbox.value = true;
-needVerticalScalingCheckbox.value = false;
+var faceDirectionCheckBox_MG = manualCropSettingsGroup.add("checkbox", undefined, "面朝方向为左");
+var needDrawGuideLineCheckbox_MG = manualCropSettingsGroup.add("checkbox", undefined, "绘制出参考线");
+var needExportCSVFileCheckbox_MG = manualCropSettingsGroup.add("checkbox", undefined, "同时导出CSV");
+var needHorizontalScalingCheckbox_MG = manualCropSettingsGroup.add("checkbox", undefined, "水平方向扩展");
+var needVerticalScalingCheckbox_MG = manualCropSettingsGroup.add("checkbox", undefined, "垂直方向扩展");
+faceDirectionCheckBox_MG.value = true;
+needDrawGuideLineCheckbox_MG.value = true;
+needExportCSVFileCheckbox_MG.value = true;
+needHorizontalScalingCheckbox_MG.value = true;
+needVerticalScalingCheckbox_MG.value = false;
 
 // 裁剪当前文档 Button
 var manualCropCurrentDocumentButton = manualCropSettingsGroup.add("button", undefined, "裁剪当前文档");
@@ -111,6 +115,12 @@ manualCropCurrentDocumentButton.onClick = function()
         {
             offsetX = center.x - preCenter.x;
             offsetY = center.y - preCenter.y;
+        }
+        // 根据角色的左右朝向，决定偏移值的正负，保证偏移值的正始终代表着角色的“正面”前进方向，负代表角色的“背后”后退方向
+        if(manualCropSettings.faceLeft)
+        {
+            offsetX = -offsetX;
+            offsetY = -offsetY;
         }
         csvFile.writeln(doc.name + "," + center.x + "," + center.y + "," + offsetX + "," + offsetY);
         preCenter = center;
@@ -193,6 +203,12 @@ manualCropAllOpenedDocumentButton.onClick = function()
                 offsetX = center.x - preCenter.x;
                 offsetY = center.y - preCenter.y;
             }
+            // 根据角色的左右朝向，决定偏移值的正负，保证偏移值的正始终代表着角色的“正面”前进方向，负代表角色的“背后”后退方向
+            if(manualCropSettings.faceLeft)
+            {
+                offsetX = -offsetX;
+                offsetY = -offsetY;
+            }
             csvFile.writeln(doc.name + "," + center.x + "," + center.y + "," + offsetX + "," + offsetY);
             preCenter = center;
             ManualCropCurrentDocument(doc);
@@ -248,22 +264,24 @@ saveAllOpenedDocumentButton.onClick = function()
 // 居中锚点方式 Toggle Group
 var radioGroup = bipedAutoCropSettingsGroup.add("panel", undefined, "居中锚点方式");
 radioGroup.orientation = "row";
-var radio1 = radioGroup.add("radiobutton", undefined, "底部中心");
-var radio2 = radioGroup.add("radiobutton", undefined, "顶部中心");
-radio1.value = true;
+var radio1_BG = radioGroup.add("radiobutton", undefined, "底部中心");
+var radio2_BG = radioGroup.add("radiobutton", undefined, "顶部中心");
+radio1_BG.value = true;
 
 // 底部（顶部）容忍度 InputField 
 var inputGroup = bipedAutoCropSettingsGroup.add("group");
 inputGroup.add("statictext", undefined, "底部/顶部 范围:");
-var inputField = inputGroup.add("edittext", [0, 0, 20, 20], bipedAutoCropSettings.toleranceOfBottom);
+var inputField_BG = inputGroup.add("edittext", [0, 0, 20, 20], bipedAutoCropSettings.toleranceOfBottom);
 
 // 其它 Toggle
-var needDrawGuideLineCheckbox = bipedAutoCropSettingsGroup.add("checkbox", undefined, "绘制参考线");
-var neddCropTopCheckbox = bipedAutoCropSettingsGroup.add("checkbox", undefined, "对顶部裁剪");
-var neddCropBottomCheckbox = bipedAutoCropSettingsGroup.add("checkbox", undefined, "对底部裁剪");
-needDrawGuideLineCheckbox.value = true; // 默认选中
-neddCropTopCheckbox.value = true; // 默认选中
-neddCropBottomCheckbox.value = true; // 默认选中
+var faceDirectionCheckBox_BG = bipedAutoCropSettingsGroup.add("checkbox", undefined, "面朝方向为左");
+var needDrawGuideLineCheckbox_BG = bipedAutoCropSettingsGroup.add("checkbox", undefined, "绘制出参考线");
+var neddCropTopCheckbox_BG= bipedAutoCropSettingsGroup.add("checkbox", undefined, "裁剪顶部空白");
+var neddCropBottomCheckbox_BG = bipedAutoCropSettingsGroup.add("checkbox", undefined, "裁剪底部空白");
+faceDirectionCheckBox_BG.value = true;
+needDrawGuideLineCheckbox_BG.value = true;
+neddCropTopCheckbox_BG.value = true;
+neddCropBottomCheckbox_BG.value = true;
 
 // 裁剪当前文档 Button
 var cropCurrentDocumentButton = bipedAutoCropSettingsGroup.add("button", undefined, "裁剪当前文档");
@@ -297,6 +315,70 @@ cropAllOpenedDocumentButton.onClick = function()
         app.activeDocument = doc;
         BipedAutoCropCurrentDocument(doc);
     }
+};
+
+// 导出所有文档锚点到CSV
+var exportCSVOnlyButton = bipedAutoCropSettingsGroup.add("button", undefined, "导出所有文档锚点到CSV");
+exportCSVOnlyButton.onClick = function() 
+{
+    if (!SyncBipedAutoCropSettingsParameters()) return;
+    if (app.documents.length === 0) 
+    {
+        alert("没有打开任何文档，请先打开文档再进行裁剪");
+        return;
+    }
+
+    // 打开或创建CSV文件
+    var folder = Folder.selectDialog("选择导出文件夹");
+    if (!folder) 
+    {
+        alert("未选择导出文件夹，取消裁剪");
+        return;
+    }
+    var csvFile = new File(folder.fsName + "/offsets.csv");
+    var fileExists = csvFile.exists;
+    csvFile.open("a");  // 追加打开文件，如果不存在则自动创建
+    if (!fileExists) 
+    {
+        csvFile.writeln("文件名,中心点X,中心点Y,偏移量X,偏移量Y");
+    }
+    
+    var docs = app.documents;
+    var preCenter = null;
+    for (var i = 0; i < docs.length; i++) 
+    {
+        var doc = docs[i];
+        app.activeDocument = doc;
+        try{
+            var currentLayer = doc.activeLayer;
+            var currentLayerBounds = currentLayer.bounds;
+            var center= CenterOfBound (currentLayerBounds);
+        }catch(e){
+            alert (e.message);
+        }
+        var offsetX, offsetY;
+        if(preCenter==null)
+        {
+            offsetX = 0;
+            offsetY = 0;
+        } 
+        else
+        {
+            offsetX = center.x - preCenter.x;
+            offsetY = center.y - preCenter.y;
+        }
+        // 根据角色的左右朝向，决定偏移值的正负，保证偏移值的正始终代表着角色的“正面”前进方向，负代表角色的“背后”后退方向
+        if(bipedAutoCropSettings.faceLeft)
+        {
+            offsetX = -offsetX;
+            offsetY = -offsetY;
+        }
+        csvFile.writeln(doc.name + "," + center.x + "," + center.y + "," + offsetX + "," + offsetY);
+        preCenter = center;
+    }
+    // 写入结束，关闭文件
+    if (csvFile) 
+        csvFile.close();
 };
 
 // 保存所有打开文档 Button
@@ -333,12 +415,13 @@ win.show();
 // 同步自动裁剪的参数
 function SyncBipedAutoCropSettingsParameters()
 {
-    bipedAutoCropSettings.needDrawGuideLine = needDrawGuideLineCheckbox.value;
-    bipedAutoCropSettings.needCropTop = neddCropTopCheckbox.value;
-    bipedAutoCropSettings.needCropBottom = neddCropBottomCheckbox.value;
-    bipedAutoCropSettings.isBottomCenter = radio1.value;
+    bipedAutoCropSettings.faceLeft = faceDirectionCheckBox_BG.value;
+    bipedAutoCropSettings.needDrawGuideLine = needDrawGuideLineCheckbox_BG.value;
+    bipedAutoCropSettings.needCropTop = neddCropTopCheckbox_BG.value;
+    bipedAutoCropSettings.needCropBottom = neddCropBottomCheckbox_BG.value;
+    bipedAutoCropSettings.isBottomCenter = radio1_BG.value;
     
-    var userInput = inputField.text;
+    var userInput = inputField_BG.text;
     var integerInput = parseInt(userInput, 10);
 
     if (!isNaN(integerInput) && userInput>=0 ) {
@@ -353,10 +436,11 @@ function SyncBipedAutoCropSettingsParameters()
 // 同步手动选区裁剪的参数
 function SyncManualCropSettingsParameters()
 {
-    manualCropSettings.needDrawGuideLine = needDrawGuideLineCheckbox.value;
-    manualCropSettings.needExportCSVFile = needExportCSVFileCheckbox.value;
-    manualCropSettings.needHorizontalScaling = needHorizontalScalingCheckbox.value;
-    manualCropSettings.needVerticalScaling= needVerticalScalingCheckbox.value;
+    manualCropSettings.faceLeft = faceDirectionCheckBox_MG.value;
+    manualCropSettings.needDrawGuideLine = needDrawGuideLineCheckbox_MG.value;
+    manualCropSettings.needExportCSVFile = needExportCSVFileCheckbox_MG.value;
+    manualCropSettings.needHorizontalScaling = needHorizontalScalingCheckbox_MG.value;
+    manualCropSettings.needVerticalScaling= needVerticalScalingCheckbox_MG.value;
 }
 
 // 获取选取中心点
@@ -597,6 +681,13 @@ function BipedAutoCropCurrentDocument(doc, csvFile)
 
     var centerX = (finalCropLeftPosition + finalCropRightPosition) * 0.5;
     var centerY = (finalCropToptPosition + finalCropBottomPosition) * 0.5;
+    
+    // 根据角色的左右朝向，决定偏移值的正负，保证偏移值的正始终代表着角色的“正面”前进方向，负代表角色的“背后”后退方向
+    if(bipedAutoCropSettings.faceLeft)
+    {
+        offsetX = -offsetX;
+        offsetY = -offsetY;
+    }
     csvFile.writeln(doc.name + "," + centerX  + "," + centerY + "," + offsetX + "," + offsetY);
     
     // 设置裁剪区域
